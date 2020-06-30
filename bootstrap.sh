90cecf92cfeba35108d1941f90b952b71d3af2ad
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2020-06-30 13:50:06 +0300

    Move to zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,19 +4,19 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install software
-brew install git-crypt bash-completion pinentry-mac python mc gnupg terraform packer packer-completion wget vim bash git
-brew cask install docker mactex
+brew install pinentry-mac python mc gnupg terraform packer packer-completion wget vim zsh git lazygit
+brew cask install docker slack telegram dozer
 
 # Install vim plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-ln -s ~/Projects/Public/DotFiles/.profile ~/.profile
-ln -s ~/Projects/Public/DotFiles/.bashrc ~/.bashrc
-ln -s ~/Projects/Public/DotFiles/.zshrc ~/.zshrc
-ln -s ~/Projects/Public/DotFiles/.gitconfig ~/.gitconfig
-ln -s ~/Projects/Public/DotFiles/.gitcommit ~/.gitcommit
-ln -s ~/Projects/Public/DotFiles/.bash_functions ~/.bash_functions
-ln -s ~/Projects/Public/DotFiles/.bash_aliases ~/.bash_aliases
-ln -s ~/Projects/Public/DotFiles/.bash_completion ~/.bash_completion
-ln -s ~/Projects/Public/DotFiles/.vimrc ~/.vimrc
+# Install OhMyZSH
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+ln -sf ~/Projects/Public/DotFiles/.profile ~/.profile
+ln -sf ~/Projects/Public/DotFiles/.bashrc ~/.bashrc
+ln -sf ~/Projects/Public/DotFiles/.zshrc ~/.zshrc
+ln -sf ~/Projects/Public/DotFiles/.gitconfig ~/.gitconfig
+ln -sf ~/Projects/Public/DotFiles/.gitcommit ~/.gitcommit
+ln -sf ~/Projects/Public/DotFiles/.vimrc ~/.vimrc
