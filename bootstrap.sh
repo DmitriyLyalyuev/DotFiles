24d67c77118074161084f08beffdd86f64cbcf79
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2021-03-03 08:51:41 +0200

    add tools

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+arch --x86_64 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install software
-brew install pinentry-mac python mc gnupg terraform packer packer-completion wget vim zsh git lazygit
-brew cask install docker slack telegram dozer
+arch --x86_64 brew install pinentry-mac python mc gnupg terraform packer packer-completion wget vim zsh git lazygit pre-commit tflint
+arch --x86_64 brew cask install docker slack telegram
 
 # Install vim plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -15,7 +15,7 @@ vim +PluginInstall +qall
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 ln -sf ~/Projects/Public/DotFiles/.profile ~/.profile
-ln -sf ~/Projects/Public/DotFiles/.bashrc ~/.bashrc
+#ln -sf ~/Projects/Public/DotFiles/.bashrc ~/.bashrc
 ln -sf ~/Projects/Public/DotFiles/.zshrc ~/.zshrc
 ln -sf ~/Projects/Public/DotFiles/.gitconfig ~/.gitconfig
 ln -sf ~/Projects/Public/DotFiles/.gitcommit ~/.gitcommit
